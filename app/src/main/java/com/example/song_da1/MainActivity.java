4e0adf9b2bf115742b6d05fb90799f2c05bdbcca
lakshya0414 <lakshyavaswani@gmail.com>
2022-01-25 18:28:58 +0530

    Music Player_da1

---
+++ app/src/main/java/com/example/song_da1/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.song_da1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    MediaPlayer music;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        music = MediaPlayer.create(
+                this,R.raw.song);
+    }
+    public void musicplay(View v)
+    {
+        music.start();
+    }
+
+    // Pausing the music
+    public void musicpause(View v)
+    {
+        music.pause();
+    }
+
+    // Stoping the music
+    public void musicstop(View v)
+    {
+        music.stop();
+        music
+                = MediaPlayer.create(
+                this, R.raw.song);
+    }
+}
